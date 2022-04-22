7942bc23601b58b13267a76477648c78076eb92e
Lakorthus <julio28ve@gmail.com>
2022-04-22 17:01:43 +0100

    Game1

---
+++ SpaceInvaders/EnemyController.js
@@ -0,0 +1,127 @@
+import Enemy from "./Enemy.js";
+import MovingDirection from "./MovingDirection.js";
+
+export default class EnemyController {
+    enemyMap = [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [2, 2, 2, 3, 3, 3, 3, 2, 2, 2],
+        [2, 2, 2, 3, 3, 3, 3, 2, 2, 2],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+    ];
+
+    enemyRow = [];
+
+    currentDirection = MovingDirection.right;
+    xVelocity = 0;
+    yVelocity = 0;
+    defaultXVelocity = 0;
+    defaultYVelocity = 1;
+    moveDownTimerDefault = 30;
+    moveDownTimer = this.moveDownTimerDefault;
+    fireBulletTimerDefault = 100;
+    fireBulletTimer = this.fireBulletTimer;
+
+    constructor(canvas, enemyBulletController) {
+        this.canvas = canvas;
+        this.enemyBulletController = enemyBulletController;
+        this.createEnemies();
+    }
+
+    draw(ctx) {
+        this.decrementMoveTimer();
+        this.updateVelocityAndDirection();
+        this.drawEnemies(ctx);
+        this.resetMoveDownTimer();
+        this.fireBullet();
+    
+    }
+
+    fireBullet() {
+        this.fireBulletTimer--;
+        if(this.fireBulletTimer <= 0) {
+            this.fireBulletTimer = this.fireBulletTimerDefault;
+            const allEnemies = this.enemyRow.flat();
+            const enemyIndex = Math.floor(Math.random() * allEnemies.length);
+            const enemy = allEnemies[enemyIndex];
+            this.enemyBulletController.shoot(this.x + enemy.width / 2, this.y, -3);
+            console.log(enemyIndex)
+        }
+    }
+
+    resetMoveDownTimer() {
+        if (this.moveDownTimer <= 0) {
+            this.moveDownTimer = this.moveDownTimerDefault;
+        }
+    }
+
+    decrementMoveTimer() {
+        if (this.currentDirection === MovingDirection.downLeft || 
+            this.currentDirection === MovingDirection.downRight
+        ) {
+            this.moveDownTimer--;
+        }
+    }
+
+    updateVelocityAndDirection(){
+        for (const enemyRow of this.enemyRow) {
+            if (this.currentDirection == MovingDirection.right) {
+                this.xVelocity = this.defaultXVelocity;
+                this.yVelocity = 0;
+                const rightMostEnemy = enemyRow[enemyRow.length - 1];
+                if (rightMostEnemy.x + rightMostEnemy.width >= this.canvas.width) {
+                    this.currentDirection = MovingDirection.downLeft;
+                    break;
+                    
+                }
+            }   else if (this.currentDirection === MovingDirection.downLeft) {
+                if (this.moveDown(MovingDirection.left)) {
+                    break;
+                }          
+            }   else if (this.currentDirection == MovingDirection.left) {
+                this.xVelocity = -this.defaultXVelocity;
+                this.yVelocity = 0;
+                const leftMostEnemy = enemyRow[0];
+                if (leftMostEnemy.x <= 0) {
+                    this.currentDirection = MovingDirection.downRight;
+                    break;
+                }
+            }   else if (this.currentDirection === MovingDirection.downRight) {
+                if (this.moveDown(MovingDirection.right)) {
+                    break;
+                } 
+            }
+        }
+    }
+
+    moveDown(newDirection) {
+        this.xVelocity = 0;
+        this.yVelocity = this.defaultYVelocity;
+        if (this.moveDownTimer <= 0) {
+            this.currentDirection = newDirection;
+            return true;
+        }
+        return false;
+    }
+    
+    drawEnemies(ctx){
+        this.enemyRow.flat().forEach((enemy) => {
+            enemy.move(this.xVelocity, this.yVelocity);
+            enemy.draw(ctx);
+        })
+    }
+
+    createEnemies() {
+        this.enemyMap.forEach((row, rowIndex) => {
+            this.enemyRow[rowIndex] = [];
+            row.forEach((enemyNumber, enemyIndex) =>{
+                if (enemyNumber > 0){
+                    this.enemyRow[rowIndex].push(
+                        new Enemy (enemyIndex * 50, rowIndex * 40, enemyNumber)
+                     );
+                }
+            });
+        });
+    }
+}
